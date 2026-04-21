a14b881e34477710770755d193e6f4f93d0f1a2d
Legoman0701 <alexandre.noirtat@gmail.com>
2026-04-21 12:01:33 +0200

    light modif to the reward

--- tools/nn_trainer.py
+++ tools/nn_trainer.py
@@ -697,10 +697,8 @@ class TrainingEnv:
         fwd = c.vx * math.cos(c.angle) + c.vy * math.sin(c.angle)
         reward = 0.0
         reward += seg_advance * 5.0
-        if fwd > 30:
-            reward += fwd * 0.02
-        if fwd < 0:
-            reward -= abs(fwd) * 0.1
+        reward += fwd * 0.02
+        
         if c.drift_ratio > 0.3 and fwd > 80:
             reward += c.drift_ratio * 0.5
 
